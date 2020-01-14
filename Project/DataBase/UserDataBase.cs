04e1e6ca31f7d8f2dbb2e666b2f8a5b4dc0b5f7f
Adir <adirangel2@gmail.com>
2020-01-14 23:14:27 +0200

    MVC Project

---
+++ Project/DataBase/UserDataBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Project.Models;
+using System.Data.Entity;
+
+namespace Project.DataBase
+{
+    public class UserDataBase : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Account>().ToTable("Project");
+        }
+
+        public DbSet<Account> Users { get; set; }
+    }
+}
---
+++ Project/ViewModel/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.ViewModel
+{
+    public class UserViewModel
+    {
+        public Account userName { get; set; }
+        public List<Account> usersName { get; set; }
+    }
+}
